b4011a47af4e46a87da14c981d3d97312027bf80
michael mayobre <mbmayobre@gmail.com>
2023-10-04 21:57:48 -0400

    installed tailwindcss and react-router-dom

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+}
+
